e60154d7beb0acb5960076c6bd585294b4d617d6
Tuncay Ekmez <tuncayekmez@gmail.com>
2024-09-04 21:34:13 +0300

    feat: Add press outside the modal for close

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,4 +1,9 @@
-import { Modal as RNModal, View, Platform } from "react-native";
+import {
+  Modal as RNModal,
+  View,
+  Platform,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { BlurView } from "expo-blur";
 
 type ModalProps = {
@@ -9,22 +14,19 @@ type ModalProps = {
 
 const Modal = ({ visible, onClose, children }: ModalProps) => {
   return (
-    <RNModal
-      visible={visible}
-      onRequestClose={onClose}
-      transparent={true}
-      animationType="fade"
-    >
-      <BlurView
-        intensity={Platform.OS === "ios" ? 20 : 10}
-        className="flex-1"
-        tint="dark"
-        experimentalBlurMethod="dimezisBlurView"
-      >
-        <View className="flex-1 justify-center items-center p-4 bg-[#37415199] ">
-          {children}
-        </View>
-      </BlurView>
+    <RNModal visible={visible} transparent={true} animationType="fade">
+      <TouchableWithoutFeedback onPress={onClose}>
+        <BlurView
+          intensity={Platform.OS === "ios" ? 20 : 10}
+          className="flex-1"
+          tint="dark"
+          experimentalBlurMethod="dimezisBlurView"
+        >
+          <View className="flex-1 justify-center items-center p-4 bg-[#37415199] ">
+            {children}
+          </View>
+        </BlurView>
+      </TouchableWithoutFeedback>
     </RNModal>
   );
 };
